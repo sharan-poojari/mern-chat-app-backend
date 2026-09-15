200e53465e3d01fe9cc0d7187dbf8bb732455eb2
Sharan <poojarisharancr7@gmail.com>
2026-09-15 15:38:35 +0530

    Intial backend commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json({ limit: "10mb" })); // base64 images ke liye limit badhai
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL (Day 3 mein Vite use karenge)
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+  connectDB();
+});
