b6156da757843abfc474a48c7d809d1da88cad48
Sharan <poojarisharancr7@gmail.com>
2026-09-16 18:33:02 +0530

    Fix message controller & Add cloudinary integration

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js"; 
 
 dotenv.config();
 
-const app = express();
+
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json({ limit: "10mb" }));
@@ -24,7 +25,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
   connectDB();
 });
